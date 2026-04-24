bf381fbbb34ca2565e33b9b3ead705e8571e69a7
DiegoRenovato <diegorenovatormz@gmail.com>
2026-04-23 18:16:15 -0600

    Se agregaron las funciones buscarItem y modificarItem. Se creó una funcion menuBuscar utilizada en buscarItem. Se modificó el menú.

--- gestionBiblioteca.cpp
+++ gestionBiblioteca.cpp
@@ -26,6 +26,9 @@ void agregarItem(Item **coleccion, int *cantidad, int n);
 void mostrarItems(Item *coleccion, int cantidad);
 void guardarArchivo(Item *coleccion, int cantidad);
 void cargarArchivo(Item **coleccion, int *cantidad);
+void menuBuscar(Item *coleccion, int cantidad);
+int buscarItem(Item *coleccion, int cantidad, int tipoBusqueda, void *valor);
+void modificarItem(Item *coleccion, int cantidad);
 
 int main(int argc, char *argv[]){
     Item *coleccion = NULL;
@@ -50,7 +53,7 @@ void menu(Item **coleccion, int *cantidad){
     do{
         printf("MENU\n\n");
         printf("1. Agregar Nuevo Item\n");
-        printf("2. Buscar Item (No disponible por el momento)\n");
+        printf("2. Buscar Item\n");
         printf("3. Mostrar Todos\n");
         printf("4. Modificar Item (No disponible por el momento)\n");
         printf("5. Eliminar Item (No disponible por el momento)\n");
@@ -66,7 +69,8 @@ void menu(Item **coleccion, int *cantidad){
             break;
 
         case 2:
-            printf("Buscar Item (No disponible por el momento)\n");
+            //printf("Buscar Item (No disponible por el momento)\n");
+            menuBuscar(*coleccion, *cantidad);
             break;
 
         case 3:
@@ -75,7 +79,8 @@ void menu(Item **coleccion, int *cantidad){
             break;
 
         case 4:
-            printf("Modificar Item (No disponible por el momento)\n");
+            //printf("Modificar Item (No disponible por el momento)\n");
+            modificarItem(*coleccion, *cantidad);
             break;
 
         case 5:
@@ -229,4 +234,166 @@ void cargarArchivo(Item **coleccion, int *cantidad){
 
     fclose(f);
     printf("Datos cargados correctamente\n");
+}
+
+int buscarItem(Item *coleccion, int cantidad, int tipoBusqueda, void *valor){
+    
+    for(int i = 0; i < cantidad; i++){
+
+        switch(tipoBusqueda){
+        //Buscar en ID
+        case 1:
+            if(coleccion[i].id == *(int*)valor)
+                return i;   //Retornar posicion del arreglo de estructuras donde se haya encontrado el item
+            break;
+
+        //Buscar en Titulo
+        case 2:
+            if(strcmp(coleccion[i].titulo, (char*)valor) == 0)
+                return i;   //Retornar posicion del arreglo de estructuras donde se haya encontrado el item
+            break;
+
+        //Buscar en Autor
+        case 3:
+            if(strcmp(coleccion[i].autor, (char*)valor) == 0)
+                return i;   //Retornar posicion del arreglo de estructuras donde se haya encontrado el item
+            break;
+
+        //Buscar en Genero
+        case 4:
+            if(strcmp(coleccion[i].genero, (char*)valor) == 0)
+                return i;   //Retornar posicion del arreglo de estructuras donde se haya encontrado el item
+            break;
+
+        //Buscar en Estado
+        case 5:
+            if(coleccion[i].estado == *(int*)valor)
+                return i;   //Retornar posicion del arreglo de estructuras donde se haya encontrado el item
+            break;
+        }
+    }
+
+    return -1;
+}
+
+void menuBuscar(Item *coleccion, int cantidad){
+    int opc;
+
+    do{
+        printf("\n=== BUSCAR ITEM ===\n");
+        printf("1. Por ID\n");
+        printf("2. Por Titulo\n");
+        printf("3. Por Autor\n");
+        printf("4. Por Genero\n");
+        printf("5. Por Estado\n");
+        printf("6. Regresar al menu principal\n");
+        printf("Seleccione una opcion: ");
+        scanf("%d", &opc);
+
+        if(opc == 6){
+            return;     //Regresar a menu principal
+        }
+
+        int pos = -1;
+
+        //Buscar Item por ID
+        if(opc == 1){
+            int id;
+            printf("Ingrese ID: ");
+            scanf("%d", &id);
+            pos = buscarItem(coleccion, cantidad, 1, &id);
+        }
+
+        //Buscar Item por Titulo, Autor ó Genero
+        else if(opc >= 2 && opc <= 4){
+            char texto[100];
+            getchar();
+            printf("Ingrese texto: ");
+            fgets(texto, 100, stdin);
+            texto[strcspn(texto, "\n")] = '\0';
+
+            pos = buscarItem(coleccion, cantidad, opc, texto);
+        }
+
+        //Buscar Item por Estado
+        else if(opc == 5){
+            int estado;
+            printf("Estado (0=Disponible, 1=Prestado): ");
+            scanf("%d", &estado);
+            pos = buscarItem(coleccion, cantidad, 5, &estado);
+        }
+
+        //Mostrar la informacion del item encontrado
+        if(pos != -1){
+            printf("\nItem encontrado:\n");
+            printf("ID: %d\n", coleccion[pos].id);
+            printf("Titulo: %s\n", coleccion[pos].titulo);
+            printf("Autor: %s\n", coleccion[pos].autor);
+            printf("Anio: %d\n", coleccion[pos].anio);
+            printf("Genero: %s\n", coleccion[pos].genero);
+            printf("Estado: %s\n", coleccion[pos].estado == DISPONIBLE ? "Disponible" : "Prestado");
+        } else {
+            printf("Item no encontrado\n");
+        }
+
+    }while(1);
+}
+
+void modificarItem(Item *coleccion, int cantidad){
+    int id, pos;
+
+    printf("Ingrese el ID del item a modificar: ");
+    scanf("%d", &id);
+
+    //Reutilizar la funcion buscarItem y guardar la posicion donde se encuentre el Item
+    pos = buscarItem(coleccion, cantidad, 1, &id);
+
+    if(pos == -1){
+        printf("Item no encontrado\n");
+        return;
+    }
+
+    //Mostrar los datos del Item encontrado
+    printf("\nItem encontrado:\n");
+    printf("Titulo: %s\n", coleccion[pos].titulo);
+    printf("Autor: %s\n", coleccion[pos].autor);
+    printf("Anio: %d\n", coleccion[pos].anio);
+    printf("Genero: %s\n", coleccion[pos].genero);
+    printf("Estado: %s\n", coleccion[pos].estado == DISPONIBLE ? "Disponible" : "Prestado");
+
+    printf("\n--- Ingrese los nuevos datos ---\n");
+
+    //Nuevo titulo
+    printf("Nuevo titulo: ");
+    getchar();
+    fgets(coleccion[pos].titulo, TAM_TITULO, stdin);
+    coleccion[pos].titulo[strcspn(coleccion[pos].titulo, "\n")] = '\0'; //Eliminar "\n" del buffer
+
+    //Nuevo autor
+    printf("Nuevo autor: ");
+    fgets(coleccion[pos].autor, TAM_AUTOR, stdin);
+    coleccion[pos].autor[strcspn(coleccion[pos].autor, "\n")] = '\0';   //Eliminar "\n" del buffer
+
+    //Nuevo anio
+    printf("Nuevo anio: ");
+    scanf("%d", &coleccion[pos].anio);
+
+    //Nuevo genero
+    printf("Nuevo genero: ");
+    getchar();
+    fgets(coleccion[pos].genero, TAM_GENERO, stdin);
+    coleccion[pos].genero[strcspn(coleccion[pos].genero, "\n")] = '\0';
+
+    //Nuevo estado
+    int estado;
+    do{
+        printf("Nuevo estado (0=Disponible, 1=Prestado): ");
+        scanf("%d", &estado);
+    }while(estado != 0 && estado != 1);
+
+    coleccion[pos].estado = (Estado)estado;
+
+    printf("Item modificado correctamente\n");
+
+    guardarArchivo(coleccion, cantidad);    //Guardar los datos modificados
 }
